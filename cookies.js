3d00325c9145874c4d108b3f01d82d540f214580
cheddarmcmelt <gadc3110@gmail.com>
2025-12-09 09:53:49 -0300

    Add files via upload
    javascript da mensagem de cookies (em construção)

---
+++ cookies.js
@@ -0,0 +1,14 @@
+// em construção
+
+var msgCookies = getElementById('cookies-msg')
+
+function aceito(){
+    localStorage.lgpd = "sim"
+    msgCookies.classList.remove('mostrar')
+}
+
+if(localStorage.lgpd == 'sim'){
+    msgCookies.classList.remove('mostrar')
+}else{
+    msgCookies.classList.add('mostrar')
+}
